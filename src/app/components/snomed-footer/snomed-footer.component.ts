2a0de8cba616c945a0249498386628b7adcb9651
Peter Duff <peterduff@gmail.com>
2022-09-07 11:10:10 +0100

    eslint cleaning

--- src/app/components/snomed-footer/snomed-footer.component.ts
+++ src/app/components/snomed-footer/snomed-footer.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-snomed-footer',
     templateUrl: './snomed-footer.component.html',
     styleUrls: ['./snomed-footer.component.scss']
 })
-export class SnomedFooterComponent implements OnInit {
+export class SnomedFooterComponent {
 
     year: number = new Date().getFullYear();
     constructor() {
-    }
-
-    ngOnInit() {
     }
 
 }
